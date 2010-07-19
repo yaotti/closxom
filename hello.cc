4b89cf7ef8bdfca7cd915ccf6c2a1aa4ce02c5ec
Hiroshige Umino <hiroshige88@gmail.com>
2010-07-19 18:07:55 +0900

    master: path名を変更

--- hello.cc
+++ hello.cc
@@ -3,19 +3,12 @@
 using namespace std;
 using namespace closxom;
 
+static string datarootpath("/PATH/TO/DATA/DIR/");
+
 int main(int argc, char **argv)
 {
-    const Config config("/Users/hiroshige/active/intern-prep/p/closxom/test-files/");
-
+    const Config config(datarootpath);
     Closxom *closxom = new Closxom(config);
-    //cout << "content-type: text/html" << endl << endl;
     closxom->Dispatch();
     return 0;
 }
-
-
-
-
-
-
-
